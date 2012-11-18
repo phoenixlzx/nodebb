4fb622c848465dda6dcb16f0793733ff6c880aa6
Phoenix Nemo <mrphoenixlzx@gmail.com>
2012-11-18 22:29:56 +0800

    add some routes

--- routes.js
+++ routes.js
@@ -3,9 +3,27 @@ var bbs = require('./routes/bbs');
     admin = require('./routes/admin');
 
 module.exports = function(app) {
+    // BBS routes.
+    // TODO merge bbs.auth_permission with user.auth
     app.get('/', bbs.index);
     app.get('/forum/:forumid', bbs.viewForum);
     app.get('/topic/:topicid', bbs.viewTopic);
     app.get('/topic/:topicid/edit', bbs.auth_permission, bbs.editTopic);
     app.post('/topic/:topicid/edit', bbs.auth_permission, bbs.editTopic);
     app.post('/topic/:topicid/delete', bbs.auth_permission, bbs.delTopic);
+    app.get('/member/:userid', bbs.auth_permission, bbs.viewProfile);
+
+    // user routes. Need idea for user permission check.
+    app.get('/reg', user.auth, user.reg);
+    app.post('/reg', user.auth, user.reg);
+    app.get('/login', user.auth, user.login);
+    app.post('/login', user.auth, user.login);
+    app.get('/logout', user.auth, user.logout);
+    app.get('/ucp', user.auth, user.control);
+    app.post('/ucp', user.auth, user.control);
+
+    // admin routes.
+    app.get('/admin', admin.auth, admin.panel);
+    app.post('/admin', admin.auth, admin.panel);
+
+}
