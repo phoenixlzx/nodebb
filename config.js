053ca30649724f9d6a72893e28d3e1d4a0ee01d5
Phoenix Nemo <mrphoenixlzx@gmail.com>
2012-11-19 15:53:16 +0800

    edited config.js

--- config.js
+++ config.js
@@ -1,7 +1,30 @@
 var mongodb = require('mongodb');
 
-// Site settings here.
+exports.config = {
+    name: 'NodeBB',
+    description: 'A lightweight forum software powered by Express & MongoDB',
+    version: '0.0.1',
+    
+    // Site settings
+    host: 'http://yourdomain.tld',
+    logo: '',
+    port: 8000
+    
+    rss: {
+        title: 'NodeBB',
+        link: 'http://yourdomain.tld',
+        language: 'zh-cn',
+        description: 'A lightweight forum software powered by Express & MongoDB',
+        max_items: '20'
+    };
+
+    // Mail service settings
+    mail_port: 25,
+    mail_name: 'user@mail.tld'
 
 
-// Database settings
-var mongoStore = require('connect-mongo')(express);
+    // Database settings
+    session_secret: 'session_secret',
+    auth_cookie_name: 'cookie_auth',
+    var mongoStore = require('connect-mongo')(express);
+};
